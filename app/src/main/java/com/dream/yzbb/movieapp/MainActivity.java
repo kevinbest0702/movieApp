1b4743382a9458711ff47c95a64dfef118096a40
Xianpeng <xianpzha@cisco.com>
2016-06-18 17:40:35 +0800

    try to use data bind MVVM model

--- app/src/main/java/com/dream/yzbb/movieapp/MainActivity.java
+++ app/src/main/java/com/dream/yzbb/movieapp/MainActivity.java
@@ -1,13 +1,35 @@
 package com.dream.yzbb.movieapp;
 
+import com.dream.yzbb.movieapp.databinding.ActivityMainBinding;
+import com.dream.yzbb.movieapp.entity.User;
+
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mButton;
+    private TextView mTextView;
+    private User mUser = new User();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActivityMainBinding activityMainBinding = DataBindingUtil.bind(findViewById(R.id.root_view));
+        mButton = (Button) findViewById(R.id.action);
+        mTextView = (TextView) findViewById(R.id.content);
+//        ActivityMainBinding activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        activityMainBinding.setUser(mUser);
+    }
+
+    public void assignValue(View view) {
+        Toast.makeText(this, "Set user's name", Toast.LENGTH_SHORT).show();
+        mUser.setName("Xianpzha");
+//        mUser.name.set("Xianpzha");
     }
 }
---
+++ app/src/main/java/com/dream/yzbb/movieapp/entity/User.java
@@ -0,0 +1,52 @@
+package com.dream.yzbb.movieapp.entity;
+
+import com.dream.yzbb.movieapp.BR;
+
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+import android.databinding.ObservableField;
+
+/**
+ * Created by kevin on 16/6/18.
+ */
+public class User extends BaseObservable {
+//public class User{
+//    public final ObservableField<String> name = new ObservableField<>();
+    private String name;
+    private int age;
+
+
+//    public void setName(String name) {
+//        this.name.set(name);
+//    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public User() {
+    }
+
+    @Bindable
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+        notifyPropertyChanged(BR.name);
+    }
+
+//    public User(String name, int age) {
+//        this.name.set(name);
+//        this.age = age;
+//    }
+//
+//    public User(String name) {
+//        this.name.set(name);
+//    }
+}
